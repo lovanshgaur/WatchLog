bb1558910e86909779c207c95460a55295ee9e77
prynkagupta <priyanka2002.pkg@gmail.com>
2026-01-30 20:25:22 +0530

    add JS toggle logic between signin and signup forms

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,14 @@
-// init 
-let loader = document.getElementById('preloader');
-window.addEventListener('load',()=>{
-    loader.style.display ="none"
-})
+const container = document.querySelector('.auth-container');
+const signupBtn = document.getElementById('signup-btn');
+const signinBtn = document.getElementById('signin-btn');
+
+signupBtn.addEventListener('click', (e) => {
+  e.preventDefault(); 
+  container.classList.add('active-signup');
+});
+
+signinBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  container.classList.remove('active-signup');
+});
+
