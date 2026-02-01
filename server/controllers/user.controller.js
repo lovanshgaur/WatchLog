ed2cbf20844b33b1f5b3a6d17a68b0e62edf5a39
lovanshgaur <lovanshgaur@gmail.com>
2026-02-01 14:54:02 +0530

    feat - change password controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from '../models/User.model.js';
+import bcrypt from 'bcrypt';
 
 export const changeAvatar = async (req, res) => {
     try {
@@ -26,6 +27,42 @@ export const changeAvatar = async (req, res) => {
 }
 
 
-export const changePassword = async (req,res) => {
-    res.send('Change Password')
+export const changePassword = async (req, res) => {
+    try {
+        const { oldPassword, newPassword } = req.body;
+
+        if (!oldPassword || !newPassword) {
+            res.status(401).json({ message: `All fields are mandatory.` })
+        }
+
+        if (oldPassword === newPassword) {
+            res.status(401).json({ message: `Old Password can't be same as New Password` })
+        }
+
+        const user = await User.findById(req.user.id).select("+password");
+        console.log(user)
+        if (!user) {
+            res.status(401).json({ message: `User not found!` })
+        }
+
+        const isMatched = await bcrypt.compare(oldPassword, user.password);
+        if (!isMatched) {
+            return res.status(400).json({
+                message: "Old Password is incorrect."
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10)
+        user.password = hashedPassword;
+        await user.save()
+
+        return res.json({
+            message: "Password updated successfully."
+        })
+
+
+    } catch (error) {
+        console.log(`Change Password error : ${error}`)
+        res.status(500).json({ message: `Change password error : ${error}` })
+    }
 }
