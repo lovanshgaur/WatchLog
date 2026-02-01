983f0889cbf9ddf5d4c09acb074215f279be3ad7
lovanshgaur <lovanshgaur@gmail.com>
2026-02-01 14:51:25 +0530

    feat - sign in controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/User.model.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
+// signup function
 export const signup = async (req, res) => {
     try {
         const { username, email, password } = await req.body;
@@ -23,17 +25,46 @@ export const signup = async (req, res) => {
         await User.create({
             username, email, password: hashedPassword
         });
-        res.status(201).json({ message: "User created successfully!", "User": { username, email, hashedPassword } })
+        res.status(201).json({ message: "User created successfully!", "User": { username, email, password } })
 
     } catch (error) {
         res.status(500).json({ message: `Signup Error: ${error}` })
-        console.log(`Signup Error: ${error}`)
+        console.log(`Signup Error: ${error}`);
     }
 }
 export const signin = async (req, res) => {
     try {
-        res.send('signin')
+        const { username, password } = await req.body;
+
+        if (!username || !password) {
+            return res.status(400).json({ message: "All fields are required." })
+        };
+
+        const user = await User.findOne({ username })
+        if (!user) {
+            return res.status(409).json({ message: "User does not exist." })
+        };
+
+        const isPassword = await bcrypt.compare(password, user.password);
+        if (!isPassword) {
+            return res.status(401).json({ message: "Password is incorrect!" });
+        };
+        
+        const token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "10d" }
+        );
+        res.json({
+            token,
+            user: {
+                id: user._id,
+                username: user.username,
+                avatar: user.avatar
+            }
+        })
+
     } catch (error) {
-        console.log(error)
+        console.log(`Signin Error: ${error}`);
     }
 }
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 import { signup, signin } from '../controllers/auth.controller.js'
 
 router.post("/signup", signup);
-router.get("/signin", signin);
+router.post("/signin", signin);
 
 export default router;
